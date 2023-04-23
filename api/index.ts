e0e724b13c881e640e34d6497710dc2397e4e493
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-23 19:56:27 +0800

    feat: use user & recipe routes

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,14 @@
 import express, { Express, Request, Response } from "express";
 
 import { init } from "./services/database";
+import user from "./routes/user";
+import recipe from "./routes/recipe";
 
 const app: Express = express();
 
 app.use(init);
+app.use(user);
+app.use(recipe);
 
 app.get("/api", (req: Request, res: Response) => {
   res.json({ message: "welcome" });
