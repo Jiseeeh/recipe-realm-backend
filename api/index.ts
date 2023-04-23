88d9d43cfa569d4aa2a195b5bbbf97ab2f3feeeb
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-23 17:09:03 +0800

    feat: use init db

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,10 @@
 import express, { Express, Request, Response } from "express";
 
+import { init } from "./services/database";
+
 const app: Express = express();
+
+app.use(init);
 
 app.get("/api", (req: Request, res: Response) => {
   res.json({ message: "welcome" });
