fa99ba1b209d585233cd3e5d79244ab882732c67
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-23 17:54:53 +0800

    feat: read multiple migrations

--- api/services/database.ts
+++ api/services/database.ts
@@ -14,15 +14,14 @@ const pool: Pool = mysql
   .promise();
 
 function init(_, __, next: NextFunction) {
-  // read multiple files if migrations > 1
-  fs.readFile(
-    path.join("api", "migrations", "init.sql"),
-    "utf-8",
-    (_, data) => {
+  const files = ["user_init.sql", "recipe_init.sql"];
+
+  for (const file of files) {
+    fs.readFile(path.join("api", "migrations", file), "utf-8", (_, data) => {
       pool.query(data);
-      next();
-    }
-  );
+    });
+  }
+  next();
 }
 
 export { pool, init };
