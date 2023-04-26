96fc58b930f8485002fcaadb88254582acf4e18b
Jiseeeh <johncarlo.camara1@gmail.com>
2023-04-26 15:07:36 +0800

    feat: add check/login user control

--- api/controller/userController.js
+++ api/controller/userController.js
@@ -1,15 +1,35 @@
 import { pool } from "../services/database";
 
 export async function createUser(req, res) {
-  const { username } = req.query;
+  const { username, password } = req.query;
 
   try {
-    const result = await pool.query("INSERT INTO user (name) VALUES (?)", [
-      username,
-    ]);
+    const result = await pool.query(
+      "INSERT INTO user (name,password) VALUES (?,?)",
+      [username, password]
+    );
 
-    res.status(201).json({ id: result[0].insertId, username, success: true });
+    res.status(200).json({
+      message: "Sign up success!",
+      id: result[0].insertId,
+      username,
+      success: true,
+    });
   } catch (error) {
-    res.status(500).json({ error, success: false });
+    res.status(500).json({ message: "Name is already taken.", success: false });
   }
+}
+
+export async function checkUser(req, res) {
+  const { username, password } = req.query;
+
+  const result = await pool.query(
+    "SELECT id,name FROM user WHERE name=? AND password=?",
+    [username, password]
+  );
+
+  // has match
+  if (result[0].length >= 1)
+    res.status(200).json({ message: "Login Success!", success: true });
+  else res.status(404).json({ message: "Login Failed", success: false });
 }
