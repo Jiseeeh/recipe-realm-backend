c694588c9e463c64a67c295c7be799c43f6f82e9
Jiseeeh <johncarlo.camara1@gmail.com>
2023-05-01 15:24:40 +0800

    feat: add admin route

--- api/index.js
+++ api/index.js
@@ -3,11 +3,13 @@ import express from "express";
 import { init } from "./services/database";
 import user from "./routes/user";
 import recipe from "./routes/recipe";
+import admin from "./routes/admin";
 
 const app = express();
 
 app.use(user);
 app.use(recipe);
+app.use(admin);
 
 app.get("/api", async (req, res) => {
   try {
