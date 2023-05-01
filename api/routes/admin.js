d0c2107e91de461ce3ec7e0e27c290e954881fd1
Jiseeeh <johncarlo.camara1@gmail.com>
2023-05-01 20:37:53 +0800

    feat: add admin routes

---
+++ api/routes/admin.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+import {
+  approveRecipe,
+  disapproveRecipe,
+  deleteRecipe,
+  bulkApprove,
+  bulkDisapprove,
+  bulkDelete,
+} from "../controller/adminController";
+
+const app = express();
+
+app.route("/api/admin/bulkApprove").patch(bulkApprove);
+app.route("/api/admin/bulkDisapprove").patch(bulkDisapprove);
+app.route("/api/admin/bulkDelete").delete(bulkDelete);
+
+app.route("/api/admin/approve/:id").patch(approveRecipe);
+app.route("/api/admin/disapprove/:id").patch(disapproveRecipe);
+
+app.route("/api/admin/recipe/:id").delete(deleteRecipe);
+
+module.exports = app;
