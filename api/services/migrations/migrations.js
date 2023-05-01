d273f827cf75f0c679ef590907f966c8d328e49f
Jiseeeh <johncarlo.camara1@gmail.com>
2023-05-01 21:41:52 +0800

    feat: add migrations as txts

---
+++ api/services/migrations/migrations.js
@@ -0,0 +1,22 @@
+export const migrations = [
+  `CREATE TABLE IF NOT EXISTS user (
+      id INT AUTO_INCREMENT,
+      name VARCHAR(50) UNIQUE NOT NULL,
+      password VARCHAR(50) NOT NULL,
+      is_admin BOOLEAN NOT NULL DEFAULT FALSE,
+      PRIMARY KEY(id)
+  );`,
+  `CREATE TABLE IF NOT EXISTS recipe(
+    id INT AUTO_INCREMENT,
+    private_id VARCHAR(255) NOT NULL,
+    name VARCHAR(75) NOT NULL,
+    author_id INT,
+    author_name VARCHAR(50) NOT NULL,
+    is_pending BOOLEAN NOT NULL DEFAULT TRUE,
+    image_link VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    ingredients TEXT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(author_id) REFERENCES user(id)
+  );`,
+];
