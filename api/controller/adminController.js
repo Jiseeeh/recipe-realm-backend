f5865ed1479a44c14fa19a7a19f8a55580d93340
Jiseeeh <johncarlo.camara1@gmail.com>
2023-06-03 20:08:11 +0800

    feat!: migrate queries to mssql

--- api/controller/adminController.js
+++ api/controller/adminController.js
@@ -1,26 +1,26 @@
-import { pool } from "../services/database";
+import { sqlQuery } from "../services/database";
 
 export async function getRecipes(req, res, next) {
-  // check if user is really an admin
   const { id, username: name } = req.query;
 
-  const user = await pool.query(
-    "SELECT is_admin FROM user WHERE id=? AND name=?",
-    [id, name]
+  // check if user is really an admin
+  const user = await sqlQuery(
+    `SELECT is_admin FROM users WHERE id=${id} AND name='${name}'`
   );
 
   const result = user[0];
 
-  if (result.length === 0) {
+  if (!result) {
     const err = new Error();
 
     err.response = { message: "Invalid Request", clearCache: true };
     err.statusCode = 404;
 
     next(err);
+    return;
   }
 
-  const isAdmin = !!user[0][0].is_admin;
+  const isAdmin = result.is_admin;
 
   if (!isAdmin) {
     const err = new Error();
@@ -31,62 +31,74 @@ export async function getRecipes(req, res, next) {
     err.statusCode = 403;
 
     next(err);
+    return;
+  }
+
+  const recipes = await sqlQuery("SELECT * FROM recipe");
+  const err = new Error();
+
+  if (recipes.length >= 1) {
+    res.status(200).json(recipes);
   } else {
-    const result = await pool.query("SELECT * FROM recipe");
-    const recipes = result[0];
-    const err = new Error();
+    err.response = { message: "No recipes found!" };
+    err.statusCode = 404;
 
-    if (recipes.length >= 1) {
-      res.status(200).json(recipes);
-    } else {
-      err.response = { message: "No recipes found!" };
-      err.statusCode = 404;
-
-      next(err);
-    }
+    next(err);
   }
 }
 
 export async function approveRecipe(req, res, next) {
   const { id } = req.params;
 
-  try {
-    await pool.query("UPDATE recipe SET is_pending = FALSE WHERE id=?", [id]);
+  const result = await sqlQuery(
+    `UPDATE recipe SET is_pending = 0 WHERE id=${id}`,
+    true
+  );
 
-    res.status(200).json({ message: "Approved!", success: true });
-  } catch {
-    // re throw
-    next(new Error());
+  if (result.rowsAffected[0] === 0) {
+    const err = new Error();
+    err.response = { message: "No recipe was found with that id." };
+    err.statusCode = 400;
+    next(err);
+    return;
   }
+
+  res.status(200).json({ message: "Approved!", success: true });
 }
 
 export async function disapproveRecipe(req, res, next) {
   const { id } = req.params;
 
-  try {
-    await pool.query("UPDATE recipe SET is_pending = TRUE WHERE id=?", [id]);
+  const result = await sqlQuery(
+    `UPDATE recipe SET is_pending = 1 WHERE id=${id}`,
+    true
+  );
 
-    res.status(200).json({ message: "Success!", success: true });
-  } catch {
-    // re throw
-    next(new Error());
+  if (result.rowsAffected[0] === 0) {
+    const err = new Error();
+    err.response = { message: "No recipe was found with that id." };
+    err.statusCode = 400;
+    next(err);
+    return;
   }
+
+  res.status(200).json({ message: "Success!", success: true });
 }
 
 export async function deleteRecipe(req, res, next) {
   const { id } = req.params;
 
-  const result = await pool.query("DELETE FROM recipe WHERE id=?", [id]);
+  const result = await sqlQuery(`DELETE FROM recipe WHERE id=${id}`, true);
 
-  if (result[0].affectedRows === 1)
-    res.status(200).json({ message: "Delete success!", success: true });
-  else {
+  if (result.rowsAffected[0] === 0) {
     const err = new Error();
-    err.response = { message: "No recipe found with that id" };
-    err.statusCode = 404;
-
+    err.response = { message: "No recipe was found with that id." };
+    err.statusCode = 400;
     next(err);
+    return;
   }
+
+  res.status(200).json({ message: "Delete success!", success: true });
 }
 
 export async function bulkApprove(req, res, next) {
@@ -95,11 +107,25 @@ export async function bulkApprove(req, res, next) {
   ids = JSON.parse(ids);
 
   try {
+    const success = [];
+    const failed = [];
+
     for (const id of ids) {
-      await pool.query("UPDATE recipe SET is_pending = FALSE WHERE id=?", [id]);
+      const result = await sqlQuery(
+        `UPDATE recipe SET is_pending = 0 WHERE id=${id}`,
+        true
+      );
+
+      if (result.rowsAffected[0] === 1) success.push(id);
+      else failed.push(id);
     }
 
-    res.status(200).json({ message: "Approved!", success: true });
+    res.status(200).json({
+      message: "Approved!",
+      success: true,
+      succeed: success.length,
+      failed: failed.length,
+    });
   } catch {
     // re throw
     next(new Error());
@@ -112,11 +138,25 @@ export async function bulkDisapprove(req, res, next) {
   ids = JSON.parse(ids);
 
   try {
+    const success = [];
+    const failed = [];
+
     for (const id of ids) {
-      await pool.query("UPDATE recipe SET is_pending = TRUE WHERE id=?", [id]);
+      const result = await sqlQuery(
+        `UPDATE recipe SET is_pending = 1 WHERE id=${id}`,
+        true
+      );
+
+      if (result.rowsAffected[0] === 1) success.push(id);
+      else failed.push(id);
     }
 
-    res.status(200).json({ message: "Success!", success: true });
+    res.status(200).json({
+      message: "Success!",
+      success: true,
+      succeed: success.length,
+      failed: failed.length,
+    });
   } catch {
     // re throw
     next(new Error());
@@ -129,11 +169,22 @@ export async function bulkDelete(req, res, next) {
   ids = JSON.parse(ids);
 
   try {
+    const success = [];
+    const failed = [];
+
     for (const id of ids) {
-      await pool.query("DELETE FROM recipe WHERE id=?", [id]);
+      const result = await sqlQuery(`DELETE FROM recipe WHERE id=${id}`, true);
+
+      if (result.rowsAffected[0] === 1) success.push(id);
+      else failed.push(id);
     }
 
-    res.status(200).json({ message: "Delete success!", success: true });
+    res.status(200).json({
+      message: "Delete success!",
+      success: true,
+      succeed: success.length,
+      failed: failed.length,
+    });
   } catch {
     // re throw
     next(new Error());
