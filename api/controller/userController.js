1888acfb0445e5a434fbdd099d9945a3d960d506
Jiseeeh <johncarlo.camara1@gmail.com>
2023-06-05 16:32:08 +0800

    feat: hash password before storing to db

--- api/controller/userController.js
+++ api/controller/userController.js
@@ -1,63 +1,85 @@
+import bcrypt from "bcrypt";
+
 import { sqlQuery } from "../services/database";
 
-// TODO: add bcrypt
 export async function createUser(req, res, next) {
   const { username, password } = req.query;
 
-  try {
-    const result = await sqlQuery(
-      `INSERT INTO users (name,password) VALUES ('${username}','${password}')`
-    );
-
-    res.status(200).json({
-      message: "Sign up success!",
-      // username,
-      success: true,
-    });
-  } catch (error) {
-    if (error.code === "EREQUEST") {
-      const err = new Error();
-      err.response = { message: "Name is already taken." };
-      err.statusCode = 409;
-
-      next(err);
-    } else {
-      const err = new Error();
-      err.response = { message: "Server is down at the moment." };
-
-      next(err);
+  bcrypt.hash(password, Number(process.env.SALT_ROUNDS), async (err, hash) => {
+    if (err) {
+      next(new Error());
+      return;
     }
-  }
+
+    try {
+      await sqlQuery(
+        `INSERT INTO users (name,password) VALUES ('${username}','${hash}')`
+      );
+
+      res.status(200).json({
+        message: "Sign up success!",
+        // username,
+        success: true,
+      });
+    } catch (error) {
+      if (error.code === "EREQUEST") {
+        const err = new Error();
+        err.response = { message: "Name is already taken." };
+        err.statusCode = 409;
+
+        next(err);
+      } else {
+        const err = new Error();
+        err.response = { message: "Server is down at the moment." };
+
+        next(err);
+      }
+    }
+  });
 }
 
 export async function checkUser(req, res, next) {
   const { username, password } = req.query;
 
-  const err = new Error();
-
   try {
+    // get user
     const result = await sqlQuery(
-      `SELECT id,name,is_admin FROM users WHERE name='${username}' AND password='${password}'`
+      `SELECT * FROM users WHERE name = '${username}'`
     );
+    const user = result[0];
 
-    // has match
-    if (result.length >= 1)
-      res.status(200).json({
-        message: "Login Success!",
-        result: { ...result[0] },
-        success: true,
-      });
-    else {
-      err.response = { message: "Login Failed" };
+    if (!user) {
+      const err = new Error();
+      err.response = { message: "User was not found." };
       err.statusCode = 404;
-
       next(err);
+      return;
     }
-  } catch (error) {
-    // re throw
-    const err = new Error();
-    err.response = { message: "Server is down at the moment." };
 
-    next(err);
+    bcrypt.compare(password, user.password, async (err, result) => {
+      if (err) {
+        next(err);
+        return;
+      }
+
+      if (!result) {
+        const err = new Error();
+        err.response = { message: "Wrong Credentials." };
+        err.statusCode = 401;
+        next(err);
+        return;
+      }
+
+      res.status(200).json({
+        message: "Login success!",
+        result: {
+          id: user.id,
+          name: user.name,
+          is_admin: user.is_admin,
+        },
+      });
+    });
+  } catch (error) {
+    next(error);
   }
 }
