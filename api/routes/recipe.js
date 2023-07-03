1cbb355dba630953e80ab2656350fb0cb46c875d
Jiseeeh <johncarlo.camara1@gmail.com>
2023-07-03 14:03:19 +0800

    feat: add dislike route

--- api/routes/recipe.js
+++ api/routes/recipe.js
@@ -9,6 +9,7 @@ import {
   updateRecipe,
   likeRecipe,
   getRecipeLikes,
+  dislikeRecipe,
 } from "../controller/recipeController";
 
 const app = express();
@@ -23,6 +24,11 @@ app
 app.route("/api/recipe/:id/:username").get(getRecipesByUser);
 
 // recipe likes
-app.route("/api/recipe-likes").get(getRecipeLikes).post(likeRecipe);
+// recipe likes
+app
+  .route("/api/recipe-likes")
+  .get(getRecipeLikes)
+  .post(likeRecipe)
+  .patch(dislikeRecipe);
 
 module.exports = app;
